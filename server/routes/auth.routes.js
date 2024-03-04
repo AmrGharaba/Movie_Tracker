12f6b98443cca1473713e0085269b46fde06f962
Shahrazad Masalmah <shahrazadmasalmah@gmail.com>
2024-03-04 10:18:04 +0200

    change auth routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,9 +2,9 @@ const AuthController = require("../controllers/auth.controller");
 
 module.exports = app => {
     app.post('/api/register', AuthController.register);
-    app.post("/api/register_admin", AuthController.registerAdmin);
+    app.post("/api/admin/register", AuthController.registerAdmin);
     app.post("/api/login", AuthController.login);
-    app.post("/api/admin_login", AuthController.adminLogin);
+    app.post("/api/admin/login", AuthController.adminLogin);
     app.post("/api/logout", AuthController.logout);
     app.post("/api/refresh_token", AuthController.generateAccessToken);
 }
